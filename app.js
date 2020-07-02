f8c816f8760584b7644d4f09471320f1777b9993
Justin Huang <justinhuang777@gmail.com>
2020-07-02 20:54:11 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = 3000
@@ -9,6 +11,18 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'JustinTheDriver',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 
 app.listen(port, () => {
   db.sequelize.sync()
--- controllers/userController.js
+++ controllers/userController.js
@@ -8,14 +8,54 @@ let userController = {
   },
 
   register: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    })
-    .then(user => {
-      return res.redirect('/register')
-    })
+    const { name, email, password, confirmPassword } = req.body
+    const errors = []
+
+    if (!email && !password && !confirmPassword) {
+      errors.push({ message: 'Please fill in Email, password and confirm password.' })
+    } else if (!email) {
+      errors.push({ message: 'Please fill in Email.' })
+    } else if (!password) {
+      errors.push({ message: 'Please fill in password.' })
+    } else if (!confirmPassword) {
+      errors.push({ message: 'Please fill in confirm password.' })
+    }
+
+    if ((password.length && confirmPassword.length) && (password !== confirmPassword)) {
+      errors.push({ message: 'Password or confirm password incorrect.' })
+    }
+
+    if (errors.length) {
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword
+      })
+    }
+
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) {
+          errors.push({ message: 'This email is registered.' })
+          return res.render('register', {
+            name,
+            email,
+            password,
+            confirmPassword
+          })
+        }
+        return User.create({
+          name,
+          email,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
+        }).then(user => {
+          req.flash('success_msg', 'Register successfully.')
+          return res.render('register')
+        })
+      })
+
   }
 }
 
