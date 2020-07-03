1c0af45d0a0048de4cd7a4f0a5446869e7c38bcf
Justin Huang <justinhuang777@gmail.com>
2020-07-03 21:31:21 +0800

    feat: add user seed data

---
+++ seeders/20200703132323-my-seed-file.js
@@ -0,0 +1,33 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: bcrypt.hashSync('123', bcrypt.genSaltSync(10), null),
+      isAdmin: true,
+      name: 'root',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: bcrypt.hashSync('123', bcrypt.genSaltSync(10), null),
+      isAdmin: false,
+      name: 'user1',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: bcrypt.hashSync('123', bcrypt.genSaltSync(10), null),
+      isAdmin: false,
+      name: 'user2',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+};
