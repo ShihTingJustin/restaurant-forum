75726650927f376bd13590af8ae9c5f60916dced
Justin Huang <justinhuang777@gmail.com>
2020-07-07 16:54:48 +0800

    feat: add category seed data

--- seeders/20200703132323-my-seed-file.js
+++ seeders/20200703132323-my-seed-file.js
@@ -1,10 +1,11 @@
 'use strict';
 const bcrypt = require('bcryptjs')
-const faker = require('faker')
+const faker = require('faker');
+
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-     queryInterface.bulkInsert('Users', [{
+    queryInterface.bulkInsert('Users', [{
       email: 'root@example.com',
       password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
       isAdmin: true,
@@ -20,6 +21,17 @@ module.exports = {
       updatedAt: new Date()
     }], {})
 
+    queryInterface.bulkInsert('Categories',
+      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理'].map((item, index) =>
+        ({
+          id: index + 1,
+          name: item,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        })
+      )
+    )
+
     return queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }).map(d =>
         ({
@@ -30,13 +42,15 @@ module.exports = {
           image: 'https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}',
           description: faker.lorem.text(),
           createdAt: new Date(),
-          updatedAt: new Date()
+          updatedAt: new Date(),
+          CategoryId: Math.floor(Math.random() * 5) + 1
         })
       ), {})
   },
 
   down: (queryInterface, Sequelize) => {
-   queryInterface.bulkDelete('Users', null, {})
-   return queryInterface.bulkDelete('Restaurants', null, {})
+    queryInterface.bulkDelete('Users', null, {})
+    queryInterface.bulkDelete('Categories', null, {})
+    return queryInterface.bulkDelete('Restaurants', null, {})
   }
 };
