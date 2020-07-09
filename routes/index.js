b0b98fad0d930ef67cffe66054147ec7370685a0
Justin Huang <justinhuang777@gmail.com>
2020-07-10 00:40:51 +0800

    feat: add user profile edit feature

--- routes/index.js
+++ routes/index.js
@@ -34,6 +34,7 @@ module.exports = (app, passport) => {
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
+  app.put('/users/:id', authenticated, userController.putUser)
 
   // 後台頁面
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
