786e9a822ee811fe4dc5aa170893d11bf60ea4ea
Justin Huang <justinhuang777@gmail.com>
2020-07-09 15:46:37 +0800

    feat: show related comments on restaurant page

--- controllers/restController.js
+++ controllers/restController.js
@@ -1,6 +1,8 @@
 const db = require('../models')
 const Restaurant = db.Restaurant
 const Category = db.Category
+const Comment = db.Comment
+const User = db.User
 
 const pageLimit = 10
 
@@ -16,10 +18,10 @@ const restController = {
       categoryId = Number(req.query.categoryId)
       whereQuery['categoryId'] = categoryId
     }
-    Restaurant.findAndCountAll({ 
-      raw: true, 
-      nest: true, 
-      include: Category, 
+    Restaurant.findAndCountAll({
+      raw: true,
+      nest: true,
+      include: Category,
       where: whereQuery,
       offset,
       limit: pageLimit
@@ -27,7 +29,7 @@ const restController = {
       .then(result => {
         let page = Number(req.query.page) || 1
         let pages = Math.ceil(result.count / pageLimit)
-        let totalPage = Array.from({length: pages}).map((item, index) => index + 1)
+        let totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
         let prev = page - 1 < 1 ? 1 : page - 1
         let next = page + 1 > pages ? pages : page + 1
         console.log(result)
@@ -54,8 +56,16 @@ const restController = {
 
   getRestaurant: (req, res) => {
     const { id } = req.params
-    return Restaurant.findByPk(id, { include: Category })
-      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+    return Restaurant.findByPk(id, {
+      include: [
+        Category,
+        { model: Comment, include: [User] }
+      ]
+    })
+      .then(restaurant => {
+        console.log(restaurant.Comments[0].dataValues)
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
   }
 
 
