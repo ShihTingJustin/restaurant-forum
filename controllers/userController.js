dd82e7da776a0a512142543b41dcbf56d3fd27b5
Justin Huang <justinhuang777@gmail.com>
2020-07-11 22:02:59 +0800

    feat: add topUser page

--- controllers/userController.js
+++ controllers/userController.js
@@ -202,6 +202,24 @@ let userController = {
       like.destroy()
     }).then(() => res.redirect('back'))
       .catch(err => console.log(err))
+  },
+
+  getTopUser: (req, res) => {
+    return User.findAll({
+      raw: true,
+      nest: true,
+      include: [
+        { model: User, as: 'Followers'}
+      ]
+    }).then(users => {
+      users = users.map(user => ({
+        ...user,
+        FollowerCount: user.Followers.length,
+        isFollowed: req.user.Followings.map(d => d.id).includes(user.id)
+      }))
+      users = users.sort((a, b) => b.FollowerCount - a.FollowerCount)
+      return res.render('topUser', { users })
+    }).catch(err => console.log(err))
   }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -35,6 +35,8 @@ module.exports = (app, passport) => {
   
   app.post('/comments', authenticated, commentController.postComment)
   app.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
+
+  app.get('/users/top', authenticated, userController.getTopUser)
   app.get('/users/:id', authenticated, userController.getUser)
   app.get('/users/:id/edit', authenticated, userController.editUser)
   app.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
